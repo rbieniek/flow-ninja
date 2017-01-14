29b01f63434b097826fce7a4e02e0221927da259
Rainer Bieniek <External.Rainer.Bieniek@kuehne-nagel.com>
2017-01-14 01:34:55 +0100

    Add create topic function to broker utils

--- kafka-common/src/main/java/org/flowninja/common/kafka/BrokerUtils.java
+++ kafka-common/src/main/java/org/flowninja/common/kafka/BrokerUtils.java
@@ -1,59 +1,71 @@
 package org.flowninja.common.kafka;
 
+import java.util.Optional;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
-
-import org.springframework.beans.factory.InitializingBean;
-
-import lombok.RequiredArgsConstructor;
-
-/*
-import kafka.admin.AdminUtils;
-import kafka.utils.ZKStringSerializer$;
-import kafka.utils.ZkUtils;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class KafkaTopicCreationInJava
-{
-    public static void main(String[] args) throws Exception {
-        ZkClient zkClient = null;
-        ZkUtils zkUtils = null;
-        try {
-            String zookeeperHosts = "192.168.20.1:2181"; // If multiple zookeeper then -> String zookeeperHosts = "192.168.20.1:2181,192.168.20.2:2181";
-            int sessionTimeOutInMs = 15 * 1000; // 15 secs
-            int connectionTimeOutInMs = 10 * 1000; // 10 secs
+import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode.Safe$;
+import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-            zkClient = new ZkClient(zookeeperHosts, sessionTimeOutInMs, connectionTimeOutInMs, ZKStringSerializer$.MODULE$);
-            zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperHosts), false);
-
-            String topicName = "testTopic";
-            int noOfPartitions = 2;
-            int noOfReplication = 3;
-            Properties topicConfiguration = new Properties();
-
-            AdminUtils.createTopic(zkUtils, topicName, noOfPartitions, noOfReplication, topicConfiguration);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            if (zkClient != null) {
-                zkClient.close();
-            }
-        }
-    }
-} */
 @RequiredArgsConstructor
-public class BrokerUtils implements InitializingBean {
-    private final ZookeeperClusterProperties zookeeperClusterProperties;
-    private final KafkaBrokerClusterProperties kafkaBrokerClusterProperties;
+@Slf4j
+public class BrokerUtils implements InitializingBean, DisposableBean {
+	private final ZookeeperClusterProperties zookeeperClusterProperties;
+	private final KafkaBrokerClusterProperties kafkaBrokerClusterProperties;
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-    }
+	private ZkClient zkClient = null;
+	private ZkUtils zkUtils = null;
 
-    public String bootstrapServers() {
-        return StringUtils.join(kafkaBrokerClusterProperties.getBrokers().stream().map(bp -> "PLAINTEXT://" + bp.getBindAddr().getHostAddress() + ":" + Integer.toString(bp.getPortNumber())).collect(Collectors.toList()),",");
-    }
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		try {
+			int sessionTimeOutInMs = 15 * 1000; // 15 secs
+			int connectionTimeOutInMs = 10 * 1000; // 10 secs
+
+			zkClient = new ZkClient(zookeeperServers(), sessionTimeOutInMs, connectionTimeOutInMs,
+					ZKStringSerializer$.MODULE$);
+			zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperServers()), false);
+
+		} catch (Exception ex) {
+			log.info("Failed to create zookeeper client", ex);
+
+			throw ex;
+		}
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		zkClient.close();
+	}
+
+	public void createTopic(final String topicName, final Optional<Integer> numberOfPartitions,
+			final Optional<Integer> numberOfReplicatiopn, final Optional<Properties> topicConfiguration) {
+
+		AdminUtils.createTopic(zkUtils, topicName, numberOfPartitions.orElse(1), numberOfReplicatiopn.orElse(1),
+				topicConfiguration.orElse(new Properties()), Safe$.MODULE$);
+
+	}
+
+	public String bootstrapServers() {
+		return StringUtils.join(kafkaBrokerClusterProperties.getBrokers().stream().map(
+				bp -> "PLAINTEXT://" + bp.getBindAddr().getHostAddress() + ":" + Integer.toString(bp.getPortNumber()))
+				.collect(Collectors.toList()), ",");
+	}
+
+	public String zookeeperServers() {
+		return StringUtils.join(zookeeperClusterProperties.getServers().stream()
+				.map(bp -> bp.getBindAddr().getHostAddress() + ":" + Integer.toString(bp.getPortNumber()))
+				.collect(Collectors.toList()), ",");
+	}
+
 }
--- kafka-common/src/test/java/org/flowninja/common/kafka/BrokerUtilsIntegrationTest.java
+++ kafka-common/src/test/java/org/flowninja/common/kafka/BrokerUtilsIntegrationTest.java
@@ -1,11 +1,14 @@
 package org.flowninja.common.kafka;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.net.InetAddress;
 import java.util.Collections;
+import java.util.Optional;
 
+import org.flowninja.common.TestConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -14,86 +17,100 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.SocketUtils;
 
-import org.flowninja.common.TestConfig;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.NONE, classes=BrokerUtilsIntegrationTest.TestConfiguration.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE, classes = BrokerUtilsIntegrationTest.TestConfiguration.class)
 public class BrokerUtilsIntegrationTest {
 
-    @Autowired
-    private BrokerUtils brokerUtils;
+	@Autowired
+	private BrokerUtils brokerUtils;
 
-    @Autowired
-    private KafkaBrokerHostProperties kafkaBrokerHostProperties;
+	@Autowired
+	private KafkaBrokerHostProperties kafkaBrokerHostProperties;
 
-    @Test
-    public void shouldHaveBootstraServers() {
-        assertThat(brokerUtils.bootstrapServers()).isEqualTo("PLAINTEXT://" + kafkaBrokerHostProperties.brokerAddress());
-    }
+	@Autowired
+	private ZookeeperHostProperties zookeeperHostProperties;
 
-    @TestConfig
-    public static class TestConfiguration {
-        @Bean
-        @Autowired
-        public BrokerUtils brokerUtils(final ZookeeperClusterProperties zookeeperClusterProperties, final KafkaBrokerClusterProperties kafkaBrokerClusterProperties) {
-            return new BrokerUtils(zookeeperClusterProperties, kafkaBrokerClusterProperties);
-        }
+	@Test
+	public void shouldHaveBootstrapServers() {
+		assertThat(brokerUtils.bootstrapServers())
+				.isEqualTo("PLAINTEXT://" + kafkaBrokerHostProperties.brokerAddress());
+	}
 
-        @Bean
-        public ZookeeperHostProperties zookeeperHostProperties() {
-            final ZookeeperHostProperties properties = new ZookeeperHostProperties();
+	@Test
+	public void shouldHaveZookeeperServers() {
+		assertThat(brokerUtils.zookeeperServers()).isEqualTo(zookeeperHostProperties.getBindAddr().getHostAddress()
+				+ ":" + Integer.toString(zookeeperHostProperties.getPortNumber()));
+	}
 
-            properties.setBindAddr(InetAddress.getLoopbackAddress());
-            properties.setPortNumber(SocketUtils.findAvailableTcpPort(32768));
+	@Test
+	public void shouldCreateTopic() {
+		brokerUtils.createTopic("test-topic", Optional.empty(), Optional.empty(), Optional.empty());
+	}
 
-            return properties;
-        }
+	@TestConfig
+	public static class TestConfiguration {
+		@Bean
+		@Autowired
+		public BrokerUtils brokerUtils(final ZookeeperClusterProperties zookeeperClusterProperties,
+				final KafkaBrokerClusterProperties kafkaBrokerClusterProperties) {
+			return new BrokerUtils(zookeeperClusterProperties, kafkaBrokerClusterProperties);
+		}
 
-        @Bean
-        public KafkaBrokerHostProperties kafkaBrokerHostProperties() {
-            final KafkaBrokerHostProperties properties = new KafkaBrokerHostProperties();
+		@Bean
+		public ZookeeperHostProperties zookeeperHostProperties() {
+			final ZookeeperHostProperties properties = new ZookeeperHostProperties();
 
-            properties.setBindAddr(InetAddress.getLoopbackAddress());
-            properties.setPortNumber(SocketUtils.findAvailableTcpPort(32768));
+			properties.setBindAddr(InetAddress.getLoopbackAddress());
+			properties.setPortNumber(SocketUtils.findAvailableTcpPort(32768));
 
-            return properties;
-        }
+			return properties;
+		}
 
-        @Bean
-        @Autowired
-        @DependsOn("embeddedZookeeper")
-        public EmbeddedKafkaBroker embeddedKafkaBroker(final KafkaBrokerHostProperties kafkaProperties,final ZookeeperHostProperties zookeeperProperties) {
-            return new EmbeddedKafkaBroker(kafkaProperties, zookeeperProperties);
-        }
+		@Bean
+		public KafkaBrokerHostProperties kafkaBrokerHostProperties() {
+			final KafkaBrokerHostProperties properties = new KafkaBrokerHostProperties();
 
-        @Bean
-        @Autowired
-        public EmbeddedZookeeper embeddedZookeeper(final ZookeeperHostProperties zookeeperProperties) {
-            return new EmbeddedZookeeper(zookeeperProperties);
-        }
+			properties.setBindAddr(InetAddress.getLoopbackAddress());
+			properties.setPortNumber(SocketUtils.findAvailableTcpPort(32768));
 
-        @Bean
-        @Autowired
-        public KafkaBrokerClusterProperties kafkaBrokerClusterProperties(final KafkaBrokerHostProperties kafkaBrokerHostProperties) {
-            final KafkaBrokerClusterProperties kafkaBrokerClusterProperties = new KafkaBrokerClusterProperties();
+			return properties;
+		}
 
-            kafkaBrokerClusterProperties.setBrokers(Collections.singletonList(kafkaBrokerHostProperties));
+		@Bean
+		@Autowired
+		@DependsOn("embeddedZookeeper")
+		public EmbeddedKafkaBroker embeddedKafkaBroker(final KafkaBrokerHostProperties kafkaProperties,
+				final ZookeeperHostProperties zookeeperProperties) {
+			return new EmbeddedKafkaBroker(kafkaProperties, zookeeperProperties);
+		}
 
-            return kafkaBrokerClusterProperties;
-        }
+		@Bean
+		@Autowired
+		public EmbeddedZookeeper embeddedZookeeper(final ZookeeperHostProperties zookeeperProperties) {
+			return new EmbeddedZookeeper(zookeeperProperties);
+		}
 
-        @Bean
-        @Autowired
-        public ZookeeperClusterProperties zookeeperClusterProperties(final ZookeeperHostProperties zookeeperHostProperties) {
-            final ZookeeperClusterProperties zookeeperClusterProperties = new ZookeeperClusterProperties();
+		@Bean
+		@Autowired
+		public KafkaBrokerClusterProperties kafkaBrokerClusterProperties(
+				final KafkaBrokerHostProperties kafkaBrokerHostProperties) {
+			final KafkaBrokerClusterProperties kafkaBrokerClusterProperties = new KafkaBrokerClusterProperties();
 
-            zookeeperClusterProperties.setServers(Collections.singletonList(zookeeperHostProperties));
+			kafkaBrokerClusterProperties.setBrokers(Collections.singletonList(kafkaBrokerHostProperties));
 
-            return zookeeperClusterProperties;
-        }
+			return kafkaBrokerClusterProperties;
+		}
 
+		@Bean
+		@Autowired
+		public ZookeeperClusterProperties zookeeperClusterProperties(
+				final ZookeeperHostProperties zookeeperHostProperties) {
+			final ZookeeperClusterProperties zookeeperClusterProperties = new ZookeeperClusterProperties();
 
-    }
+			zookeeperClusterProperties.setServers(Collections.singletonList(zookeeperHostProperties));
+
+			return zookeeperClusterProperties;
+		}
+
+	}
 }
